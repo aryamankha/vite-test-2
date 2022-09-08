c755df4dc90680f62e0c6a9ca3060eaf4d8eac7a
aryamankha <aryamank@princeton.edu>
2022-09-08 12:34:20 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+if (process.env.NODE_ENV === "development") {
+  import("vivid-studio").then((v) => v.run());
+  import("vivid-studio/style");
+}
 
 function App() {
   return (
     <Router>
-    <div className="pt-20">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+      <div className="pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
