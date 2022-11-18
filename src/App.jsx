f451fd117ecce99e8c21968abe339fc4ff6ae60e
Aryaman Khandelwal <62365335+aryamankha@users.noreply.github.com>
2022-11-18 12:58:43 -0500

    Remove old Vivid import

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
-if (process.env.NODE_ENV === "development") {
-  import("vivid-studio").then((v) => v.run());
-  import("vivid-studio/style");
-}
-
 function App() {
   return (
     <Router>
